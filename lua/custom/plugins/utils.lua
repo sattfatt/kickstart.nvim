d97cd7af02e5a35a6cc1acbceb26b69cbd8ebaae
Satyam Patel <satyam.patel@rockbot.com>
2024-12-08 11:27:29 -0800

    snacks!

--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -214,4 +214,30 @@ return {
       },
     },
   },
+  {
+    'folke/snacks.nvim',
+    opts = {
+      bigfile = {
+        -- your bigfile configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+    },
+    keys = {
+      {
+        '<leader>.',
+        function()
+          Snacks.scratch()
+        end,
+        desc = 'Toggle Scratch Buffer',
+      },
+      {
+        '<leader>S',
+        function()
+          Snacks.scratch.select()
+        end,
+        desc = 'Select Scratch Buffer',
+      },
+    },
+  },
 }
