5662c281ea52bc3f55752c7651ede83c99a9826b
Satyam Patel <sattfatt@gmail.com>
2025-10-07 23:01:12 -0700

    cant load this plugin for some reason

--- lua/custom/plugins/http.lua
+++ lua/custom/plugins/http.lua
@@ -1,40 +1,40 @@
-return {
-  {
-    'rest-nvim/rest.nvim',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      opts = function(_, opts)
-        opts.ensure_installed = opts.ensure_installed or {}
-        table.insert(opts.ensure_installed, 'http')
-
-        vim.g.rest_nvim = {
-          env = {
-            enable = true,
-            pattern = '.*%.env.*',
-            find = function()
-              local config = require 'rest-nvim.config'
-              return vim.fs.find(function(name, _)
-                return name:match(config.env.pattern)
-              end, {
-                path = '~/envs/http/',
-                type = 'file',
-                limit = math.huge,
-              })
-            end,
-          },
-          response = {
-            ---Default response hooks
-            hooks = {
-              decode_url = true,
-              format = true,
-            },
-          },
-        }
-
-        vim.keymap.set('n', '<leader>ao', ':vert Rest open<CR>', { desc = '[a]pi [o]pen [a]pi http result pane' })
-        vim.keymap.set('n', '<leader>ar', ':vert Rest run<CR>', { desc = '[a]pi [r]un http under cursor' })
-        vim.keymap.set('n', '<leader>ae', ':Rest env select<CR>', { desc = '[a]pi [s]elect env to use' })
-      end,
-    },
-  },
-}
+-- return {
+--   {
+--     'rest-nvim/rest.nvim',
+--     dependencies = {
+--       'nvim-treesitter/nvim-treesitter',
+--       opts = function(_, opts)
+--         opts.ensure_installed = opts.ensure_installed or {}
+--         table.insert(opts.ensure_installed, 'http')
+--
+--         vim.g.rest_nvim = {
+--           env = {
+--             enable = true,
+--             pattern = '.*%.env.*',
+--             find = function()
+--               local config = require 'rest-nvim.config'
+--               return vim.fs.find(function(name, _)
+--                 return name:match(config.env.pattern)
+--               end, {
+--                 path = '~/envs/http/',
+--                 type = 'file',
+--                 limit = math.huge,
+--               })
+--             end,
+--           },
+--           response = {
+--             ---Default response hooks
+--             hooks = {
+--               decode_url = true,
+--               format = true,
+--             },
+--           },
+--         }
+--
+--         vim.keymap.set('n', '<leader>ao', ':vert Rest open<CR>', { desc = '[a]pi [o]pen [a]pi http result pane' })
+--         vim.keymap.set('n', '<leader>ar', ':vert Rest run<CR>', { desc = '[a]pi [r]un http under cursor' })
+--         vim.keymap.set('n', '<leader>ae', ':Rest env select<CR>', { desc = '[a]pi [s]elect env to use' })
+--       end,
+--     },
+--   },
+-- }
