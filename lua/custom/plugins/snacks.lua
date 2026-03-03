a5a4a170c0240fddfc289b70faf12318c1163d61
satyam <satyam.patel@rockbot.com>
2026-03-02 21:55:25 -0800

    : update

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -22,7 +22,21 @@ return {
       gh = {},
       picker = {},
       explorer = {},
-      dashboard = {},
+      dashboard = {
+        preset = {
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = '󰋊 ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'm', desc = 'Mason', action = ':Mason' },
+            { icon = '󰩈 ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
+        },
+      },
     },
     keys = {
       {
